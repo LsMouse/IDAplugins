1367a7cb65f87f2533c180c19b7058d1c0d100a5
LsMouse <364987065>
2015-05-20 12:37:46 +0800

    https://github.com/LsMouse/IDAplugins

--- IDAplugins/IDAplugins/Util_LIB/Char/Util_Base.h
+++ IDAplugins/IDAplugins/Util_LIB/Char/Util_Base.h
@@ -1,10 +1,24 @@
 #include "Util_LIB.H"
 class Util_Base{
 public:
+/**
+* @See	获取已清空的内存
+* @Param	inSize -> 传入内存获取宽度
+* @Return	out_Mem -> 申请的内存起始地址
+*/
 	static void* Alloc(ulong inSize){
-		void* out = malloc(inSize+1);
-		memset(out, 0, inSize + 1);
-		return out;
+		void* out_Mem = malloc(inSize+1);
+		memset(out_Mem, 0, inSize + 1);
+		return out_Mem;
 	}
+/**
+* @See 
+* @Class 
+* @Struct
+* @Function 
+* @Param 
+* @Return 
+* @sSince
+*/
 };
 
